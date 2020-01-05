4997b6ddf377fa21127facbe13ad357981077189
github username <github email address>
2020-01-05 10:35:07 -0800

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -13,11 +13,12 @@ def clock_angle(time)
     hour_hand = (hour * 30) + (minute * 0.5)
     minute_hand = minute * 6
 
-    if hour == 9        #hack to pass test.  
+    if minute_hand > hour_hand
+        return minute_hand - hour_hand
+    elsif hour_hand > 0 && minute_hand == 0
         return 360 - hour_hand
     else
         return hour_hand - minute_hand
     end
-
 end
 
