c1f27d53f1cb567f497bcdafc37509fa1f4e683a
github username <github email address>
2020-01-05 22:48:12 -0800

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,22 +1,18 @@
-require 'pry'
-
 def clock_angle(time)
     hour = time.split(':')[0].to_i
     minute = time.split(':')[1].to_i
-
     hour == 12 && hour = 0
-
-    # every 60 min hour hand moves 30 degrees
-    # hour hand moves 0.5 degrees per minute
-    # minute hand moves 6 degrees per minute
-    
     hour_hand = (hour * 30) + (minute * 0.5)
     minute_hand = minute * 6
 
     if minute_hand > hour_hand
         return minute_hand - hour_hand
     elsif hour_hand > 0 && minute_hand == 0
-        return 360 - hour_hand
+        if hour_hand < 180
+            return hour_hand
+        else
+            return 360 - hour_hand
+        end
     else
         return hour_hand - minute_hand
     end
